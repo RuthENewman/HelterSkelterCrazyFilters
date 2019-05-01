9c0945bf893e8a692d7577d95bf1123e2a81dc45
Ruth Newman <ruthnewman@ruths-air.home>
2019-05-02 00:08:08 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  const takePhotoButton = document.querySelector('.takePhoto');
+  takePhotoButton.addEventListener('click', event => takePhoto);
+}
+
+
+function takePhoto() {
+
+}
